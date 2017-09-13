0ec133d4feb2b7968a898b044d5aedd2e2f9965f
Anthony deguzman110@gmail.com <deguzman110@gmail.com>
2017-09-13 16:57:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  var welcome = `Welcome, ${name}. You are number ${katzDeliLine} in line.`;
+  var i = 0;
+  var welcome = `Welcome, ${name}. You are number ${katzDeliLine[i]} in line.`;
   return welcome;
 }
