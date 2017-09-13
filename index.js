1d2f7381112619c4f6f850610d6e5376c730c729
Anthony deguzman110@gmail.com <deguzman110@gmail.com>
2017-09-13 17:11:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine, name) {
     if (katzDeliLine.length === 0) {
       var nowTaking = "There is nobody waiting to be served!";
     } else {
-      var nowTaking = `Currently serving ${name}.`;
+      var nowTaking = `Currently serving ${katzDeliLine[0]}.`;
     }  
   katzDeliLine.shift();
   return nowTaking;
